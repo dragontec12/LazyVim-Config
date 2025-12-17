85b66c15c17a9558850f12f282de382e9fccdff5
Jens Wilbert <jens.wilbert66@gmail.com>
2025-12-17 21:39:47 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 💤 LazyVim config
+
+Config of [LazyVim](https://github.com/LazyVim/LazyVim).
+
+## Keymaps
+
+| Keybinding  | Mode   | Description                                  |
+|-------------|--------|----------------------------------------------|
+| `<leader>K` | Normal | Search for Word (Google, DevDocs, Man Pages) |
+
+## Plugins
+
+- ["benomahony/uv.nvim"](https://www.github.com/benomahony/uv.nvim)
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,48 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Keymaps for Documentation search
+local actions = {
+  {
+    name = "man",
+    action = function(word)
+      vim.cmd("Man " .. word)
+    end,
+  },
+  {
+    name = "devdocs",
+    action = function(word)
+      vim.fn.jobstart({ "open", "https://devdocs.io/#q=" .. word })
+    end,
+  },
+  {
+    name = "google",
+    action = function(word)
+      vim.fn.jobstart({ "open", "https://www.google.com/search?q=" .. word })
+    end,
+  },
+}
+
+local function choose_doc()
+  local word = vim.fn.expand("<cword>")
+  if not word or word == "" then
+    vim.notify("No word selected", vim.log.levels.WARN)
+    return
+  end
+  vim.ui.select(actions, {
+    prompt = "Choose documentation",
+    format_item = function(item)
+      return item.name
+    end,
+  }, function(choice)
+    if choice and choice.action then
+      choice.action(word)
+    end
+  end)
+end
+
+vim.keymap.del("n", "<leader>K")
+
+-- Map <leader>K to new picker
+vim.keymap.set("n", "<leader>K", choose_doc, { desc = "Search in Docs" })
